2f2560f5518754b5c52b86b41850c0c4cadd3682
duaameh <mehdawiduaa@gmail.com>
2025-05-23 19:30:10 +0300

    23.5.25

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+project = {
+  title: 'Hotels Management System',
+  description: 'A full-featured hotel management system...',
+  tech: 'Angular, TypeScript, SCSS, RxJS',
+  github: 'https://github.com/YOUR_USERNAME/hotel-management-system',
+  images: [
+    '1.a.png',
+    '1.c.png',
+    '1.b.png'
+  ]
+};
+project2 = {
+  title: 'Hotels Management System',
+  description: 'A full-featured hotel management system...',
+  tech: 'Angular, TypeScript, SCSS, RxJS',
+  github: 'https://github.com/YOUR_USERNAME/hotel-management-system',
+  images: [
+    '1.a.png',
+    '1.c.png',
+    '1.b.png'
+  ]
+};
+
+currentImageIndex = 0;
+
+ngOnInit() {
+  setInterval(() => {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.project.images.length;
+  }, 1000); // تغيير الصورة كل 3 ثواني
+}
 
 }
