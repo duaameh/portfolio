a77554d03d1471bcdb484f81e28ddcb5cd2f397c
duaameh <mehdawiduaa@gmail.com>
2025-05-25 00:04:29 +0300

    Initial commit

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -7,38 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent implements OnInit {
-project = {
-  title: 'Hotels Management System',
-  description: 'A full-featured hotel management system...',
-  tech: 'Angular, TypeScript, SCSS, HTML',
-  github: 'https://github.com/yousefshatnawi/hottel-managment-system-.git',
-  Demo: 'https://lnkd.in/dwz6pF4j',
-  images: [
-    '1.a.png',
-    '1.c.png',
-    '1.b.png'
-  ]
-};
-
-project2 = {
-  title: 'MANAGEMENT SYSTEM WEBSITE ',
-  description: 'A web-based application developed using Angular that...',
-  tech: 'Angular, TypeScript, SCSS, HTML',
-  github: 'https://github.com/yousefshatnawi/managmentsystem10.git',
-  images: [
-    'project02.jpg',
-    'project03.jpg',
-    'project04.jpg',
-    'project05.jpg'
-  ]
-};
+projects = [
+  {
+    title: 'Hotels Management System',
+    description: 'A full-featured hotel management system...',
+    tech: 'Angular, TypeScript, SCSS, HTML',
+    github: 'https://github.com/yousefshatnawi/hottel-managment-system-.git',
+    Demo: 'https://lnkd.in/dwz6pF4j',
+    images: ['1.a.jpg','1.c.jpg','1.b.jpg','1.d.jpg','1.e.jpg','1.e.jpg','1.2.jpg','1.9.jpg'],
+    currentImageIndex: 0
+  },
+  {
+    title: 'Managment System Websit  ',
+    description: 'A web-based application developed using Angular that...',
+    tech: 'Angular, TypeScript, SCSS, HTML',
+    github: 'https://github.com/yousefshatnawi/managmentsystem10.git',
+    Demo: '',
+    images: ['project02.jpg', 'project03.jpg', 'project04.jpg', 'project05.jpg'],
+    currentImageIndex: 0
+  },
+  {
+    title: 'TO DO LIST ',
+    description: 'An Angular-based task management application that includes a note section ',
+    tech: 'Angular, TypeScript, SCSS, HTML',
+    github: 'https://github.com/duaameh/to-do-list',
+    Demo: '#',
+    images: ['3.a.jpg', '3.b.jpg', '3.c.jpg'],
+    currentImageIndex: 0
+  }
+  ,
+  {
+    title: 'Furniture Selling Website (Full stack WEB APP)',
+    description: 'online gifting platform, which is meant to solve the problem of how to choose the right gift It presents to the users ',
+    tech: 'HTML , CSS , JavaScript ,ASP.NET Core, ',
+    github: 'https://github.com/duaameh/HereToYouProject',
+    Demo: '#',
+    images: ['4.a.jpg', '4.b.jpg', '4.c.jpg', '4.d.jpg'],
+    currentImageIndex: 0
+  }
+];
 
 currentImageIndex = 0;
 
 ngOnInit() {
-  setInterval(() => {
-    this.currentImageIndex = (this.currentImageIndex + 1) % this.project.images.length;
-  }, 2000); // تغيير الصورة كل 3 ثواني
+  this.projects.forEach((project, index) => {
+    setInterval(() => {
+      project.currentImageIndex = (project.currentImageIndex + 1) % project.images.length;
+    }, 2000);
+  });
 }
+
 
 }
